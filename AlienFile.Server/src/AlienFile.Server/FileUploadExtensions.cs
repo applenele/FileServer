ddd4cfbcfb0fa920a4067198d5ba38ce49c9a49d
lenny <1941973283@qq.com>
2016-12-30 21:46:06 +0800

    Signed-off-by: lenny <1941973283@qq.com>

---
+++ AlienFile.Server/src/AlienFile.Server/FileUploadExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlienFile.Server
+{
+    public static class FileUploadExtensions
+    {
+        public static IApplicationBuilder UseFileUpload(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<FileUploadMiddleware>();//手工高亮
+        }
+    }
+}
---
+++ AlienFile.Server/src/AlienFile.Server/FileUploadMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using AlienFile.Server.Helper;
+
+namespace AlienFile.Server
+{
+    public class FileUploadMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private IHostingEnvironment _hostingEnv;
+
+        public FileUploadMiddleware(RequestDelegate next, IHostingEnvironment hostingEnv)
+        {
+            _next = next;
+            _hostingEnv = hostingEnv;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Method == HttpMethods.Post)
+            {
+                var files = context.Request.Form.Files;
+                foreach (var file in files)
+                {
+                    string fileName = DateHelper.GetTimeStamp() + Path.GetExtension(file.FileName);
+                    string path = Path.Combine(_hostingEnv.WebRootPath, "Upload", fileName);
+
+                    using (FileStream fs = System.IO.File.Create(path))
+                    {
+                        file.CopyTo(fs);
+                        fs.Flush();
+                    }
+                }
+            }
+            //await _next.Invoke(context);
+        }
+    }
+}
